36acca020ad2c2f6775dfd9f45776a86d14cd433
mengfanchen <mengfanchen@linkdoc.com>
2016-12-07 14:22:17 +0800

    测试

---
+++ app/src/main/java/com/and/netease/CONST.java
@@ -0,0 +1,29 @@
+package com.and.netease;
+
+public class CONST {
+
+	public static final String URL_NEWS_TOP = "http://news.163.com/special/00011K6L/rss_newstop.xml";
+	public static final String URL_NEWS_SPORT = "http://sports.163.com/special/00051K7F/rss_sportslq.xml";
+	public static final String URL_NEWS_PLAY = "http://ent.163.com/special/00031K7Q/rss_toutiao.xml";
+	public static final String URL_NEWS_FINANCE = "http://money.163.com/special/00252EQ2/yaowenrss.xml";
+	public static final String URL_NEWS_SCIENCE = "http://tech.163.com/special/000944OI/headlines.xml";
+
+	public static final String URL_NEWS_DOMESTIC = "http://news.163.com/special/00011K6L/rss_gn.xml";
+	public static final String URL_NEWS_MILITARY = "http://news.163.com/special/00011K6L/rss_war.xml";
+	public static final String URL_NEWS_INTERNATIONAL = "http://news.163.com/special/00011K6L/rss_gj.xml";
+	public static final String URL_NEWS_COMMUNITY = "http://news.163.com/special/00011K6L/rss_sh.xml";
+	public static final String URL_NEWS_DEPTH = "http://news.163.com/special/00011K6L/rss_hotnews.xml";
+	public static final String URL_NEWS_TICKET = "http://sports.163.com/special/00051K7F/rss_sportscp.xml";
+	public static final String URL_NEWS_FILM = "http://ent.163.com/special/00031K7Q/rss_entmovie.xml";
+	public static final String URL_NEWS_MUSIC = "http://ent.163.com/special/00031K7Q/rss_entmusic.xml";
+	public static final String URL_NEWS_IT = "http://tech.163.com/special/000944OI/kejiyejie.xml";
+	public static final String URL_NEWS_CAR = "http://auto.163.com/special/00081K7D/rsstoutiao.xml";
+	public static final String URL_NEWS_DIGITAL = "http://tech.163.com/digi/special/00161K7K/rss_digixj.xml";
+	
+	
+	public static final String URL_TOPIC = "http://news.163.com/special/00011K6L/rss_newsspecial.xml";
+	public static final String URL_PICTURE = "http://news.163.com/special/00011K6L/rss_photo.xml";
+	public static final String URL_FOLLOW = "";
+	public static final String URL_VOTE = "";
+	
+}
